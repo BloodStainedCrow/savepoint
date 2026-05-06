f1fb73db88720492b2e01dac7eb778cb3dc67c7c
Tris Oaten <tris@oat.sh>
2026-05-06 06:48:04 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "A command watcher that commits when you fix errors";
+
+  inputs = {
+    flakebox = {
+      url = "github:rustshop/flakebox?rev=62af969ab344229d2a0d585a482293b3f186b221";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      flakebox,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        projectName = "savepoint";
+        pkgs = flakebox.inputs.nixpkgs.legacyPackages.${system};
+
+        flakeboxLib = flakebox.lib.mkLib pkgs {
+          config = {
+            github.ci.buildOutputs = [ ".#ci.${projectName}" ];
+          };
+        };
+
+        buildPaths = [
+          "Cargo.toml"
+          "Cargo.lock"
+          "src"
+        ];
+
+        buildSrc = flakeboxLib.filterSubPaths {
+          root = builtins.path {
+            name = projectName;
+            path = ./.;
+          };
+          paths = buildPaths;
+        };
+
+        multiBuild = (flakeboxLib.craneMultiBuild { }) (
+          craneLib':
+          let
+            craneLib = (
+              craneLib'.overrideArgs {
+                pname = projectName;
+                src = buildSrc;
+                nativeBuildInputs = [ ];
+              }
+            );
+          in
+          {
+            ${projectName} = craneLib.buildPackage { };
+          }
+        );
+      in
+      {
+        packages.default = multiBuild.${projectName};
+
+        legacyPackages = multiBuild;
+
+        devShells = flakeboxLib.mkShells { };
+      }
+    );
+}
